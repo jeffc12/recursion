87cefacfae493664a22ccbefdd7577a2bcf7938e
Jeffrey Chen <jeffreychen41@gmail.com>
2017-03-19 15:49:18 -0700

    getELementsByClass Completed

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -6,5 +6,31 @@
 // But instead we're going to implement it from scratch:
 var getElementsByClassName = function(className
 ) {
+  // test for class name in
   // your code here
-};
+  //this problem must have memory
+  //since we need to look for each childNode for
+  //the className as well
+
+  var array = [];
+  // starts by inputing body for input
+  var body = document.body;
+
+  function search(name) {
+    if(name.classList) {
+      //Search for relationship with className
+      var list = name.classList.toString();
+      if (list.indexOf(className) !== -1) {
+        array.push(name);
+      }
+    }
+    // search each Child and run search to recursion to push
+    //into the array;
+      name.childNodes.forEach(function(eachChild) {
+        search(eachChild);
+    })
+  }
+   search(body);
+   return array;
+
+ }
