246f2ace1ee558a46d83601ab9b75736f0512bc6
Jeffrey Chen <jeffreychen41@gmail.com>
2017-03-19 14:13:32 -0700

    Testing for JSON Stringify Objects

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -6,21 +6,40 @@
 var stringifyJSON = function(obj) {
   // your code goes here
 //test for array
-if (Array.isArray(obj)) {
+    var array = [];
+      if (Array.isArray(obj)) {
 
-  var array = [];
-  if (obj[0] === undefined) {
-  return '['+array+']';
-}
-  obj.forEach(function(index){
+          if (obj[0] === undefined) {
+              return '['+array+']';
+            }
+            obj.forEach(function(index){
 
-    array.push(stringifyJSON(index));
+              array.push(stringifyJSON(index));
 
-  })
-  return '['+array+']';
-  //console.log(array);
-}
+            })
 
+            return '['+array+']';
+            //console.log(array);
+      }
+
+// Test if obect
+
+if (typeof obj === 'object') {
+
+
+    for (var i in obj) {
+      if (typeof(obj[i]) === 'function' || obj[i] === undefined) {
+        continue;
+      }
+
+
+      array.push(stringifyJSON(i) + ':'+stringifyJSON(obj[i]));
+    }
+    //console.log(array);
+    array = '{'+array+'}';
+
+    return array;
+  }
 
 
   // test if the object
@@ -34,13 +53,13 @@ if (Array.isArray(obj)) {
 
     return obj.toString();
   }
-  if  (typeof obj === 'boolean') {
-
+  if  (typeof obj === 'boolean' || obj === null) {
+    //console.log(obj.toString());
     return obj.toString();
   }
 
 };
 
 //test
-console.log(stringifyJSON(['hi', 0,true, -0.3, 0.3, 1343.32, 3345, 0.00011999999999999999]));
-//console.log(stringifyJSON([]));
+//console.log(stringifyJSON(['hi', 0,true, -0.3, 0.3, 1343.32, 3345, 0.00011999999999999999]));
+//console.log(stringifyJSON({'a': 'hi', 'b':true}));
